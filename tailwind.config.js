780020bd999a4079ccb1460f1433f8344674e4df
mbd <doganmbdogan@gmail.com>
2023-06-04 00:36:22 +0300

    3-Tailwindcss kuruldu ve eklemeleri yapıldı

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+//! React npx create-react-app ile kurulan projeleri tailwind in desteklemediğini söylüyor ve nextJs vb. ile bu işlmelerin yapılmasını tavsiye ediyor.
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
