9312176299b6534fcdb79997692fca15d3664423
mbd <doganmbdogan@gmail.com>
2023-06-05 10:40:41 +0300

    15-Pagination ile altta sayfa numaraları yazdırdım

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,10 +1,11 @@
 //! Burada sadece fullfilled işlemi değil pending ve rejected olayları olduğu için mutlaka loading olayını mutlaka yapmalıyım.
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../redux/ProductSlice";
 import Loading from "../Loading";
 import ProductCard from "./ProductCard";
+import ReactPaginate from "react-paginate";
 
 const Products = () => {
   //! burada dispatch ile verileri alıp UseEffect ile güncellenip çağırdıktan sonra useSelector ile gösterecem.
@@ -18,23 +19,52 @@ const Products = () => {
     dispatch(getProducts());
   }, [dispatch]);
 
-  return (
-  <div>
-    {
-      productsStatus === "LOADING" ? <Loading/> :
-      <div className="flex flex-wrap">
-       { products?.map((product,index)=> {
-        return(
-            <ProductCard key={index}  product={product} /> 
-          )
-        
-        })}
-      </div>
+  const [itemOffset, setItemOffset] = useState(0);
 
-      
-    }
-  </div>
-  )
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const itemsPerPage = 6; //! sayfamda kaç tane gözüksün istiyorum
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = products.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(products.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <div>
+      {productsStatus === "LOADING" ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="flex flex-wrap">
+            {currentItems?.map((product, index) => {
+              return <ProductCard key={index} product={product} />;
+            })}
+          </div>
+
+          <ReactPaginate
+            className="paginate"
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel="<"
+            renderOnZeroPageCount={null}
+          />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Products;
