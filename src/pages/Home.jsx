b5076435fab49ae919713b4545609698ffd74a12
mbd <doganmbdogan@gmail.com>
2023-06-05 11:05:41 +0300

    16-useState ile category i gönderdim

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SliderComponent from "../components/home/SliderComponent";
 import Sorting from "../components/home/Sorting";
 import Category from "../components/home/Category";
 import Products from "../components/home/Products";
 
 const Home = () => {
+  const [category, setCategory] = useState("");
+
   return (
     <div>
       <SliderComponent />
       <Sorting />
       <div className="flex">
-        <Category />
-        <Products />
+        <Category setCategory={setCategory} />
+        <Products category={category} />
       </div>
     </div>
   );
