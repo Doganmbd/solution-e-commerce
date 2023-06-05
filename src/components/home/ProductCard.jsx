713167aa835e2c9ce0181e1afb69a64aeec1ca39
mbd <doganmbdogan@gmail.com>
2023-06-05 15:13:04 +0300

    18- Navigate ile Detail in yolunu gösterdim

--- src/components/home/ProductCard.jsx
+++ src/components/home/ProductCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
-  const { image, price, title } = product;
+  const { image, price, title ,id } = product;
+
+  const navigate = useNavigate();
+
   return (
-    <div className="w-[400px] p-2 mx-5 mb-5 border rounded-md relative cursor-pointer ">
+    <div onClick={()=>navigate(`products/${id}`)} className="w-[400px] p-2 mx-5 mb-5 border rounded-md relative cursor-pointer ">
       <div className="text-lg font-bold absolute rounded-xl top-0 right-0 bg-black text-white p-2 m-1">
         {price} <span>TL</span>{" "}
       </div>
       <img
         className="w-[200px] h-[200px] object-cover m-auto"
         src={image}
-        alt=""
+        alt="img"
       />
       <div className="text-xl font-bold text-center px-3 m-3">{title} </div>
     </div>
