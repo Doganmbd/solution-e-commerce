9a2f6c0551043bc966066fcd0fbc63980af103f0
mbd <doganmbdogan@gmail.com>
2023-06-06 01:20:40 +0300

    26-store içine Cardslice import edildi

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CategorySlice from "./CategorySlice";
 import ProductSlice from "./ProductSlice";
+import CardSlice from "./CardSlice";
 
 export const store = configureStore({
   ///! bir Redux store’u oluştururken kullanılan bir yapılandırma objesinin bir parçasıdır. Bu obje, store’un state’ini nasıl değiştirecek olan reducer fonksiyonlarını belirtir.Reducer fonksiyonu, store’un state’ini değiştiren fonksiyondur. Reducer fonksiyonu, mevcut state’i ve dispatch edilen action’u alarak yeni state’i hesaplar ve döndürür.
   reducer: {
     categories: CategorySlice,
-    products: ProductSlice, //! Buraya import ettikten sonra yapmam gereken yine dispatch ile çağırmak, UseSelector ile çağırdıklarımı göstermek ve UseState içerisinde gelen değerleri ekrana bastırmak olacak 
+    products: ProductSlice, //! Buraya import ettikten sonra yapmam gereken yine dispatch ile çağırmak, UseSelector ile çağırdıklarımı göstermek ve UseState içerisinde gelen değerleri ekrana bastırmak olacak
+    cards: CardSlice,
   },
 });
-
-
