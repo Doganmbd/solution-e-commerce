124c3837afa2454af4dec6aaeda32fb1f7699288
mbd <doganmbdogan@gmail.com>
2023-06-05 03:38:36 +0300

    12-Products ve Loading sayfası oluşturuldu

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+//! Burada sadece fullfilled işlemi değil pending ve rejected olayları olduğu için mutlaka loading olayını mutlaka yapmalıyım.
 
 const Products = () => {
   return (
