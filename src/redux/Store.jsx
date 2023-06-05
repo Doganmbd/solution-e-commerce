de9cce4b4eee4e72dfb566c10de7048687535e78
mbd <doganmbdogan@gmail.com>
2023-06-05 03:37:19 +0300

    13-ProductSlice ı store içine import ettim

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CategorySlice from "./CategorySlice";
+import ProductSlice from "./ProductSlice";
 
 export const store = configureStore({
   ///! bir Redux store’u oluştururken kullanılan bir yapılandırma objesinin bir parçasıdır. Bu obje, store’un state’ini nasıl değiştirecek olan reducer fonksiyonlarını belirtir.Reducer fonksiyonu, store’un state’ini değiştiren fonksiyondur. Reducer fonksiyonu, mevcut state’i ve dispatch edilen action’u alarak yeni state’i hesaplar ve döndürür.
   reducer: {
     categories: CategorySlice,
+    products: ProductSlice,
   },
 });
 
