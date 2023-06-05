da6456eaddee6461f42fb0b993dbb2d093058ac0
mbd <doganmbdogan@gmail.com>
2023-06-06 01:24:04 +0300

    28-useSelector ile cards veriler alındı,kullanıldı

--- src/components/navbar/navbaritem/NavbarRight.jsx
+++ src/components/navbar/navbaritem/NavbarRight.jsx
@@ -1,8 +1,20 @@
 import { BiSearch } from "react-icons/bi";
 import { AiOutlineHeart } from "react-icons/ai";
 import { SlBasket } from "react-icons/sl";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { resultCardTotal } from "../../../redux/CardSlice";
 
 const NavbarRight = () => {
+  const dispatch = useDispatch();
+  const { cards } = useSelector((state) => state.cards);
+
+  console.log(cards);
+
+  useEffect(() => {
+    dispatch(resultCardTotal());
+  }, [dispatch]);
+
   return (
     <div className="flex items-center gap-8">
       <div className="flex items-center p-3 rounded-full bg-gray-200">
@@ -16,7 +28,7 @@ const NavbarRight = () => {
       <AiOutlineHeart size={25} />
       <div className="relative">
         <div className="absolute -top-4 -right-3 bg-red-600 text-white rounded-full w-5 h-5 flex items-center justify-center ">
-          3
+          {cards?.length}
         </div>
         <SlBasket size={25} />
       </div>
