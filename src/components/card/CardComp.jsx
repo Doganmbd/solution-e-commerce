4fa3db4f919ca9559aee40c78f7c1079720b23c5
mbd <doganmbdogan@gmail.com>
2023-06-06 02:11:07 +0300

    33-CardComp oluşturdum içini düzenledim

---
+++ src/components/card/CardComp.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CardComp = ({ card }) => {
+  const { image, title,price,quantity } = card;
+  return (
+    <div className="my-10 flex items-center justify-between">
+      <img className="w-[150px] h-[150px] object-cover" src={image} alt="i" />
+      <div className="w-[500px] ">
+        <div className="text-xl">{title} </div>
+        <div>{card?.description} </div>
+      </div>
+      <div className="text-2xl">{price} <span>TL</span> </div>
+      <div className="bg-gray-700 rounded-full text-white p-2">{quantity} </div>
+      <div className="bg-red-500 text-white w-[200px]  text-2xl cursor-pointer rounded-md h-16 flex items-center justify-center "> DELETE </div>
+    </div>
+  );
+};
+
+export default CardComp;
