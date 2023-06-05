682ce8bd1932dd08119aff93ff75e5bdfaf3a557
mbd <doganmbdogan@gmail.com>
2023-06-06 02:22:54 +0300

    34-veriyi silme işlemi yapıldı

--- src/components/card/CardComp.jsx
+++ src/components/card/CardComp.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { romoveToCard } from "../../redux/CardSlice";
 
 const CardComp = ({ card }) => {
-  const { image, title,price,quantity } = card;
+  const { image, title, price, quantity, id } = card;
+  const dispatch = useDispatch();
   return (
     <div className="my-10 flex items-center justify-between">
       <img className="w-[150px] h-[150px] object-cover" src={image} alt="i" />
@@ -9,9 +12,17 @@ const CardComp = ({ card }) => {
         <div className="text-xl">{title} </div>
         <div>{card?.description} </div>
       </div>
-      <div className="text-2xl">{price} <span>TL</span> </div>
+      <div className="text-2xl">
+        {price} <span>TL</span>{" "}
+      </div>
       <div className="bg-gray-700 rounded-full text-white p-2">{quantity} </div>
-      <div className="bg-red-500 text-white w-[200px]  text-2xl cursor-pointer rounded-md h-16 flex items-center justify-center "> DELETE </div>
+      <div
+        onClick={() => dispatch(romoveToCard(id))}
+        className="bg-red-500 text-white w-[200px]  text-2xl cursor-pointer rounded-md h-16 flex items-center justify-center "
+      >
+        {" "}
+        DELETE{" "}
+      </div>
     </div>
   );
 };
