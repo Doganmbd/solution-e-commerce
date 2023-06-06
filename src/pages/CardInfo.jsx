934ca7cb8445b6e5201b37e8b2dfe1a29ee1c617
mbd <doganmbdogan@gmail.com>
2023-06-06 11:02:20 +0300

    35-Deploy edebilmek için paketler ekledim sildim

--- src/pages/CardInfo.jsx
+++ src/pages/CardInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+
 import { resultCardTotal } from "../redux/CardSlice";
 import CardComp from "../components/card/CardComp";
 
@@ -9,8 +9,6 @@ const CardInfo = () => {
   const { cards, itemCount, totalAmount } = useSelector((state) => state.cards);
 
   console.log(cards, itemCount, totalAmount);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(resultCardTotal());
@@ -23,7 +21,10 @@ const CardInfo = () => {
           {cards.map((card, index) => (
             <CardComp key={index} card={card} />
           ))}
-          <div className="flex items-center justify-end text-2xl">TOTAL AMOUNT : <span className="font-bold text-3xl ml-2">{totalAmount}  TL</span> </div>
+          <div className="flex items-center justify-end text-2xl">
+            TOTAL AMOUNT :{" "}
+            <span className="font-bold text-3xl ml-2">{totalAmount} TL</span>{" "}
+          </div>
         </div>
       ) : (
         <div>Your cart is empty. no data available.</div>
